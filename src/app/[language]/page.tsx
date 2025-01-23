e30e6503cfb9bd13e253c1c8483a63b98430e0a4
Lucas Santos <leos.developer@gmail.com>
2025-01-23 20:09:37 -0300

    fix: fix redirect locale

--- src/app/[language]/page.tsx
+++ src/app/[language]/page.tsx
@@ -2,5 +2,5 @@ import { DEFAULT_LOCALE } from "@/constants/globals";
 import { permanentRedirect } from "next/navigation";
 
 export default function Home() {
-  permanentRedirect(`/${DEFAULT_LOCALE}/dashboard`)
+  permanentRedirect(`/${DEFAULT_LOCALE}/dashboard`);
 }
