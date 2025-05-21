e1111ce6b75dd0c885387b679535138e11fbe1f1
Lucas Santos <leos.developer@gmail.com>
2025-05-21 00:37:49 -0300

    feat: remove route group [locale]

--- src/app/[locale]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { DEFAULT_LOCALE } from "@/constants/globals";
-import { permanentRedirect } from "next/navigation";
-
-export default function MainPage() {
-  permanentRedirect(`/${DEFAULT_LOCALE}/dashboard`);
-}
